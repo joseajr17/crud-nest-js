a6c9dfc48dbfbf709dd3c65c9624d04c23dfcace
jose <josejr017@gmail.com>
2025-08-26 20:52:27 -0300

    feat: understanding the decorators of: HTTP requests, status codes, route parameters, query parameters, request body

--- src/messages/messages.controller.ts
+++ src/messages/messages.controller.ts
@@ -1,4 +1,45 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  HttpCode,
+  HttpStatus,
+  Param,
+  Post,
+  Put,
+  Query,
+} from '@nestjs/common';
 
 @Controller('messages')
-export class MessagesController {}
+export class MessagesController {
+  @HttpCode(HttpStatus.CREATED)
+  @Get()
+  findAll(@Query() pagination: any) {
+    const { limit = 10, offset = 0 } = pagination;
+    return `Lista todas as mensagens. Limit = ${limit}, Offset = ${offset}.`;
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return `Essa rota retorna a mensagem com o ID: ${id}`;
+  }
+
+  @Post()
+  create(@Body() body: any) {
+    return body;
+  }
+
+  @Put(':id')
+  update(@Param('id') id: string, @Body() body: any) {
+    return {
+      id,
+      ...body,
+    };
+  }
+
+  @Delete(':id')
+  remove(@Param('id') id: string) {
+    return `Essa rota APAGA um recado com ID: ${id}`;
+  }
+}
