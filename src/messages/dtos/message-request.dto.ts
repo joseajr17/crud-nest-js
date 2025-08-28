53bbf35338bfe568b0dc3aa8176b2eeaa3358039
jose <josejr017@gmail.com>
2025-08-28 08:33:45 -0300

    feat: validating message-request.dto data with useGlobalPipes, ValidationPipe, and class-validator

--- src/messages/dtos/message-request.dto.ts
+++ src/messages/dtos/message-request.dto.ts
@@ -1,5 +1,21 @@
+import { IsNotEmpty, IsString, MaxLength, MinLength } from 'class-validator';
+
 export class MessageRequestDTO {
+  @IsString()
+  @IsNotEmpty()
+  @MinLength(5)
+  @MaxLength(255)
   readonly text: string;
+
+  @IsString()
+  @IsNotEmpty()
+  @MinLength(3)
+  @MaxLength(50)
   readonly from: string;
+
+  @IsString()
+  @IsNotEmpty()
+  @MinLength(3)
+  @MaxLength(50)
   readonly to: string;
 }
